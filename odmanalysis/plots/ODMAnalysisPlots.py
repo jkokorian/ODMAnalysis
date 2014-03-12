c63f8cbd4738d6e5b0c634068476faa70cb486be
jkokorian <jkokorian@gmail.com>
2014-03-12 20:16:49 +0100

    animation method added to plots module

--- odmanalysis/plots/ODMAnalysisPlots.py
+++ odmanalysis/plots/ODMAnalysisPlots.py
@@ -96,6 +96,50 @@ def plotMultiCycleVoltageDisplacement(df, corrected=False, showReferenceValues=F
         
     return figure, axes
 
+def animateMultiCycleVoltageDisplacement(df, corrected=False, showReferenceValues=False, filename=None, measurementName="", nmPerPx=1, figure=None, axes=None, dpi=200):
+    
+    #create dummy axis and plot to determine xlim and ylim
+    df1 = df[df.cycleNumber ==1]
+    axDummy = plt.axes()
+    axDummy.plot(df1.actuatorVoltage,df1.displacement)
+    
+    fig = plt.figure('test')
+    ax = plt.axes(xlim=axDummy.get_xlim(), ylim=axDummy.get_ylim())
+    fwdline, = ax.plot([], [], 'b')
+    bwdline, = ax.plot([], [], 'g')
+    text = ax.set_title("")
+    
+    # initialization function: plot the background of each frame
+    def init():
+        fwdline.set_data([], [])
+        bwdline.set_data([], [])
+        text.set_text("")
+        return fwdline,bwdline,text
+    
+    # animation function.  This is called sequentially
+    def animate(i_frame):
+        print i_frame
+        dfc = df[df.cycleNumber == i_frame]
+        fwd = dfc[dfc.direction == "forward"]
+        bwd = dfc[dfc.direction == "backward"]
+        fwdline.set_data(fwd.actuatorVoltage, fwd.displacement)
+        bwdline.set_data(bwd.actuatorVoltage, bwd.displacement)
+        text.set_text("cycle %i" % i_frame)
+        return fwdline,bwdline,text,
+    
+    # call the animator.  blit=True means only re-draw the parts that have changed.
+    anim = animation.FuncAnimation(fig, animate, init_func=init,
+                                   frames=20, interval=20, blit=True)
+    
+    # save the animation as an mp4.  This requires ffmpeg or mencoder to be
+    # installed.  The extra_args ensure that the x264 codec is used, so that
+    # the video can be embedded in html5.  You may need to adjust this for
+    # your system: for more information, see
+    # http://matplotlib.sourceforge.net/api/animation_api.html
+    anim.save(filename, fps=10, extra_args=['-vcodec', 'libx264'],dpi=dpi)
+    
+    return figure, ax, anim
+
 def plotMultiCycleMeanVoltageDisplacement(df,corrected=False,showReferenceValues=False, filename=None,measurementName="", nmPerPx=1, figure=None, axes=None):
     if not figure:
         figure = plt.figure('Multiple cycle average %s (%s)' % ("(corrected)" if corrected else "", measurementName))
