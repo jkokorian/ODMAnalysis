3452a0c3e1f595b90051cc977e1edb14f64702aa
jkokorian <jkokorian@gmail.com>
2014-05-12 15:21:56 +0200

    typo fixed

--- setup.py
+++ setup.py
@@ -13,7 +13,9 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""from setuptools import setup
+"""
+
+from setuptools import setup
 
 setup(name='odmanalysis',
       version='0.5',
