0d58452331098799ae9c5df37f0266f1b9e3849c
jkokorian <jkokorian@gmail.com>
2015-04-19 22:18:19 +0200

    visual studio solution created and unit tests added for feature tracker

---
+++ odmanalysis/odmstudio/FeatureTrackerTests.py
@@ -0,0 +1,28 @@
+import unittest
+from odmstudio_lib import FeatureTracker,CurveFitTracker,FFTTracker
+
+
+
+class Test_FeatureTrackerTests(unittest.TestCase):
+    def test_TrackerRegistrations(self):
+
+        featureTrackers= FeatureTracker.getRegisteredFeatureTrackers()
+        self.assertTrue(len(featureTrackers) > 0)
+
+    def test_TrackerRetreival(self):
+        featureTrackers = FeatureTracker.getRegisteredFeatureTrackers()
+
+        tracker = featureTrackers[0]
+        print tracker
+        self.assertTrue(tracker is CurveFitTracker)
+
+    def test_DefaultDisplayName(self):
+        displayName = FeatureTracker.getDisplayName()
+        self.assertTrue(displayName is not None)
+
+    def test_CustomDisplayName(self):
+        self.assertTrue(CurveFitTracker.getDisplayName() == "Curve-fit")
+
+
+if __name__ == '__main__':
+    unittest.main()
