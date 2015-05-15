777193a6bc6fba714c1929c0e2e41e5cafd1d4ef
jkokorian <jkokorian@gmail.com>
2015-05-15 23:58:29 +0200

    very hacky first attempt, not even remotely successfull

---
+++ odmanalysis/odmstudio/odmstudio_pluginsTests.py
@@ -0,0 +1,8 @@
+import unittest
+
+class Test_odmstudio_pluginsTests(unittest.TestCase):
+    def test_getPlugins(self):
+        self.fail("Not implemented")
+
+if __name__ == '__main__':
+    unittest.main()
