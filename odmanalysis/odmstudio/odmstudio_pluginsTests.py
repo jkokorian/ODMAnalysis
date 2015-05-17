f5a4c5a404a0a5408ee5a3801517e6c56c3386b7
jkokorian <jkokorian@gmail.com>
2015-05-17 22:39:28 +0200

    working plugin 'module' directory

--- odmanalysis/odmstudio/odmstudio_pluginsTests.py
+++ odmanalysis/odmstudio/odmstudio_pluginsTests.py
@@ -1,8 +1,10 @@
 import unittest
 
 class Test_odmstudio_pluginsTests(unittest.TestCase):
-    def test_getPlugins(self):
-        self.fail("Not implemented")
+    def test_plugins_module(self):
+        from plugins.someplugin import kenker as k
+        k.knakermethod()
+        
 
 if __name__ == '__main__':
     unittest.main()
